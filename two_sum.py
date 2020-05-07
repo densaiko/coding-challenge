4a5a33782d53e53e8783b4211256b930eba83aa5
densaiko <62329779+densaiko@users.noreply.github.com>
2020-05-07 08:55:00 +0700

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -1,14 +1,14 @@
-Two sum is a simple coding challenge that we have to find the two number in list which is the sum to get result that are
-on the target.
+# Two sum is a simple coding challenge that we have to find the two number in list which is the sum to get result that are
+# on the target.
 
-The list contains [1, 8, 10, 15], and target is 18
+# The list contains [1, 8, 10, 15], and target is 18
 
-There are two method to find the two number in a list to achieve the target
-1. We can use two increment (i and j). every i in index, j will increment the rest until the two number match with the target.
-I consider on the time complexity is O(n^2) and space complexity is O(1)
+# There are two method to find the two number in a list to achieve the target
+# 1. We can use two increment (i and j). every i in index, j will increment the rest until the two number match with the target.
+# I consider on the time complexity is O(n^2) and space complexity is O(1)
 
-2. We can use one increment (i). Every i in index, the target will substract the number in each i and save it in dictionary.
-I consider on the time complexity is O(n) and space complexity is O(n)
+# 2. We can use one increment (i). Every i in index, the target will substract the number in each i and save it in dictionary.
+# I consider on the time complexity is O(n) and space complexity is O(n)
 
 class solution():
     def two_sum(self, nums, target):
