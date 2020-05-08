5a25a86e9e3977ea78843fe16e79f161ff1de0dd
densaiko <62329779+densaiko@users.noreply.github.com>
2020-05-08 08:35:51 +0700

    Create first non repeating number.py

---
+++ first non repeating number.py
@@ -0,0 +1,21 @@
+First non repeating number coding challenge is the easiest challenge in LeetCode (Correct me if I wrong)
+
+To solve this coding challenge you can make it with time complexity O(n) and space complexity O(1)
+
+Okey, lets start coding
+
+class solution():
+    def first_non_repeating(self, x, n):
+        for i in range(n):
+            j=0
+            while j<n:
+                if i!=j and x[i] == x[j]:
+                    break
+                j+=1
+            if j==n:
+                return x[i]
+        return 'a'
+    
+x = [9, 4, 9, 6, 7, 4]
+n = len(x)
+solution().first_non_repeating(x, n)
