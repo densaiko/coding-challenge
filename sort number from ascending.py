220c6784808d43d86e892b70d7dbde68f11c0895
densaiko <62329779+densaiko@users.noreply.github.com>
2020-05-09 20:35:08 +0700

    Create sort number from ascending.py

---
+++ sort number from ascending.py
@@ -0,0 +1,28 @@
+sort number from ascending is the simple coding challenge. However, it will be useful to sort the number to get
+the structured data
+
+There is lazy solution to solve this challenge. You can make new variable to sort the number in list
+
+I consider time and space complexity down below:
+- Time complexity O(n) because I only use one variable i to loop the entire number in list
+- Space complexity O(1) because I don't need an empty list to keep the number
+
+Lets find the solution
+
+class solution():
+    def sort_num_ascending(self, nums):
+        nums_one = 0
+        nums_two = 0
+        nums_three = 0
+        for n in nums:
+            if n == 1:
+                nums_one = 1 + nums_one
+            elif n == 2:
+                nums_two = 1 + nums_two
+            elif n == 3:
+                nums_three = 1 + nums_three
+                
+        return [1]*nums_one + [2]*nums_two + [3]*nums_three
+        
+nums = [3, 3, 2, 1, 3, 2, 1]
+solution().sort_num_ascending(nums)
