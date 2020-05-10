f4dc074edf6dc8675a72eebe4758199537a45d2f
densaiko <62329779+densaiko@users.noreply.github.com>
2020-05-10 08:49:02 +0700

    Update remove duplicate word.py

--- remove duplicate word.py
+++ remove duplicate word.py
@@ -1,13 +1,13 @@
-Remove duplicate word is one of medium coding challenge in LeetCode.
+# Remove duplicate word is one of medium coding challenge in LeetCode.
 
-Basically, you just take one word and insert it in new variable, after you increment and the same word, it won't append
+# Basically, you just take one word and insert it in new variable, after you increment and the same word, it won't append
 
-I consider on time and complexity to solve this coding challlenge down below
-- Time complexity, I assign i as variable that will increment every word in one way. So time complexity will be O(n)
-- Space complexity, I make new empty varible. Once I get word, it will insert it in new varibale. So space complexity will
-be O(n)
+# I consider on time and complexity to solve this coding challlenge down below
+# - Time complexity, I assign i as variable that will increment every word in one way. So time complexity will be O(n)
+# - Space complexity, I make new empty varible. Once I get word, it will insert it in new varibale. So space complexity will
+# be O(n)
 
-Okey, lets code:
+# Okey, lets code:
 
 class solution():
     def remove_duplicate_word(self, word):
