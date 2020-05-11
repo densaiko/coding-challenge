a73d60e26183b36030708be0cabb432f6678a318
densaiko <62329779+densaiko@users.noreply.github.com>
2020-05-11 20:52:57 +0700

    Update men from boys.py

--- men from boys.py
+++ men from boys.py
@@ -1,17 +1,17 @@
-Men from boys is not a game but it's one of coding challenge in medium level according to the Codewars platform. 
+# Men from boys is not a game but it's one of coding challenge in medium level according to the Codewars platform. 
 
-Here is the rules:
-- Men should be an even number and ascending
-- Boys should be an odd number and descending
+# Here is the rules:
+# - Men should be an even number and ascending
+# - Boys should be an odd number and descending
 
-The number is arranged in list [20, 33, 50, 34, 43, 46]
+# The number is arranged in list [20, 33, 50, 34, 43, 46]
 
-To solve this coding challenge. I will consider on the time and space complexity:
-- Time complexity, I have to increment all the number in a list once. So time complexity will be O(n)
-- Space complexity, I have to make new varibale boys and men to keep the number based on even and odd number. So space 
-complexity will be O(n)
+# To solve this coding challenge. I will consider on the time and space complexity:
+# - Time complexity, I have to increment all the number in a list once. So time complexity will be O(n)
+# - Space complexity, I have to make new varibale boys and men to keep the number based on even and odd number. So space 
+# complexity will be O(n)
 
-Lets code:
+# Lets code:
 class solution():
     def men_from_boys(self, arr):
         boys = []
