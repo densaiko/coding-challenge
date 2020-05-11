12e7c1a8b2e3e0a28780c3f1773de1827d540fea
densaiko <62329779+densaiko@users.noreply.github.com>
2020-05-11 20:52:36 +0700

    Create men from boys.py

---
+++ men from boys.py
@@ -0,0 +1,29 @@
+Men from boys is not a game but it's one of coding challenge in medium level according to the Codewars platform. 
+
+Here is the rules:
+- Men should be an even number and ascending
+- Boys should be an odd number and descending
+
+The number is arranged in list [20, 33, 50, 34, 43, 46]
+
+To solve this coding challenge. I will consider on the time and space complexity:
+- Time complexity, I have to increment all the number in a list once. So time complexity will be O(n)
+- Space complexity, I have to make new varibale boys and men to keep the number based on even and odd number. So space 
+complexity will be O(n)
+
+Lets code:
+class solution():
+    def men_from_boys(self, arr):
+        boys = []
+        men = []
+        for i in sorted(arr):
+            if i % 2 == 0:
+                men.append(i)
+            else:
+                boys.append(i)
+
+        return men[::1] + boys[::-1]
+
+
+arr = [20, 33, 50, 34, 43, 46]
+print(solution().men_from_boys(arr))
