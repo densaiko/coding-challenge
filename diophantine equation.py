1da880e97c351c6d76cbef4f9c21c1e3a62c7619
densaiko <62329779+densaiko@users.noreply.github.com>
2020-05-12 11:38:12 +0700

    Update diophantine equation.py

--- diophantine equation.py
+++ diophantine equation.py
@@ -1,21 +1,21 @@
-Diophantine equation is a polynomial equation. I will explain briefly what is polynomial equation. Polynomial equation contains
-two or more unknown variables. For instance x and y. In this challenge, you have to find the number of x and y that match with
-the result
+# Diophantine equation is a polynomial equation. I will explain briefly what is polynomial equation. Polynomial equation contains
+# two or more unknown variables. For instance x and y. In this challenge, you have to find the number of x and y that match with
+# the result
 
-This coding challenge I get when I faced coding interview from Devoteam. According to me, this is hard level coding challenge
-if you seldom touch polynomial equation in math field. But, I'm personally think that it is medium level.
+# This coding challenge I get when I faced coding interview from Devoteam. According to me, this is hard level coding challenge
+# if you seldom touch polynomial equation in math field. But, I'm personally think that it is medium level.
 
-x, y = (x >=0, y >=0)
+# x, y = (x >=0, y >=0)
 
-Here I give the equation and result that you have to achieve:
-X^2 - 4*Y^2 = n
+# Here I give the equation and result that you have to achieve:
+# X^2 - 4*Y^2 = n
 
-if there is no solution put []
+# if there is no solution put []
 
-solution[90005] = [[45003, 22501], [9003, 4499], [981, 467], [309, 37]]
-solution[90002] = []
+# solution[90005] = [[45003, 22501], [9003, 4499], [981, 467], [309, 37]]
+# solution[90002] = []
 
-Lets code:
+# Lets code:
 
 class solution():
     def diophantine_equation(self, n):
