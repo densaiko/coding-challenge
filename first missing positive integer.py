59f4d2bbdcf59db800155e4046d61aff64a755fa
densaiko <62329779+densaiko@users.noreply.github.com>
2020-05-14 15:34:49 +0700

    Update first missing positive integer.py

--- first missing positive integer.py
+++ first missing positive integer.py
@@ -1,14 +1,14 @@
-First missing positive integer is one of the easy coding challenge in Leetcode, HackerRank and others.
+# First missing positive integer is one of the easy coding challenge in Leetcode, HackerRank and others.
 
-Here is the list of number [1, -2, 4, 2, 5, 7]
+# Here is the list of number [1, -2, 4, 2, 5, 7]
 
-You have to find the first missing positive integer in that list.
+# You have to find the first missing positive integer in that list.
 
-The solution is so simple. I consider on the fastest solution based on time and space complexity:
-- Time complexity, I only use one variable to increment the integer. Once I got the first missing number, I stop incrementing
-- Space complexity, I don't new new list, dictionary or others to keep new integer
+# The solution is so simple. I consider on the fastest solution based on time and space complexity:
+# - Time complexity, I only use one variable to increment the integer. Once I got the first missing number, I stop incrementing
+# - Space complexity, I don't new new list, dictionary or others to keep new integer
 
-Lets code:
+# Lets code:
 
 class solution():
     def first_missing_positive_integer(self, nums):
@@ -17,5 +17,5 @@ class solution():
             if i not in nums:
                 return i
 
-nums = [-2, 1, 2, 4, 5, 7]
+nums = [1, -2, 4, 2, 5, 7]
 print(solution().first_missing_positive_integer(nums))
