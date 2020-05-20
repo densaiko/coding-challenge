680d3a121a2c18c1d62b6c3392edeab811c4c7f1
Ari Sulistiyo Prabowo <62329779+densaiko@users.noreply.github.com>
2020-05-20 11:36:04 +0700

    Update permutation.py

--- permutation.py
+++ permutation.py
@@ -1,10 +1,10 @@
-Permutation is one of coding challenge in Leetcode. In this coding challenge, we have to find the all possible permutations
-from the list given number. You can use the recursion method to answer the challenge
+# Permutation is one of coding challenge in Leetcode. In this coding challenge, we have to find the all possible permutations
+# from the list given number. You can use the recursion method to answer the challenge
 
-Time complexity will how many combinations number (N), so the time complexity will be N factorial.
-Space complexity also will save how many combinations number (N), so the space complexity will be N factorial.
+# Time complexity will how many combinations number (N), so the time complexity will be N factorial.
+# Space complexity also will save how many combinations number (N), so the space complexity will be N factorial.
 
-Lets code:
+# Lets code:
 
 class solution():
     def permute(self, nums):
