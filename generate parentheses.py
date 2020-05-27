ae2c84661f7de2181d3c3c860018284f37554754
Ari Sulistiyo Prabowo <62329779+densaiko@users.noreply.github.com>
2020-05-27 11:41:04 +0700

    Update generate parentheses.py

--- generate parentheses.py
+++ generate parentheses.py
@@ -1,15 +1,15 @@
-Generate parentheses is a medium level coding challenge in Leetcode. In this coding challenge, I have to find how many
-combinations of parentheses that are valid to be generated. 
+# Generate parentheses is a medium level coding challenge in Leetcode. In this coding challenge, I have to find how many
+# combinations of parentheses that are valid to be generated. 
 
-For instance, I give 3 pairs of parentheses and I have to find the combination of that pairs. I solve this problem by giving
-the open parentheses as a +1, once the next parentheses will give the open parentheses will be +1 and the close parentheses
-will be -1. 
+# For instance, I give 3 pairs of parentheses and I have to find the combination of that pairs. I solve this problem by giving
+# the open parentheses as a +1, once the next parentheses will give the open parentheses will be +1 and the close parentheses
+# will be -1. 
 
-To solve this problem, I consider on the time and space complexity such as
-- Time complexity will be O(n) because I use the brute force method but it's too complicated in math
-- Space complexity will be O(n) because I need variable to keep the parentheses.
+# To solve this problem, I consider on the time and space complexity such as
+# - Time complexity will be O(n) because I use the brute force method but it's too complicated in math
+# - Space complexity will be O(n) because I need variable to keep the parentheses.
 
-Lets code:
+# Lets code:
 
 class solution():
     def generateparentheses(self, n):
