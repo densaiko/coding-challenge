8f10cd8781e4747bb4dc9e44213e2b52bd6602d0
Ari Sulistiyo Prabowo <62329779+densaiko@users.noreply.github.com>
2020-06-01 07:11:54 +0700

    Update Top K Frequent Elements.py

--- Top K Frequent Elements.py
+++ Top K Frequent Elements.py
@@ -16,8 +16,8 @@ class Solution:
       heap = []
       for key, v in count.items():
           heapq.heappush(heap, (v, key))
-      if len(heap) > k:
-          heapq.heappop(heap)
+          if len(heap) > k:
+              heapq.heappop(heap)
       res = []
       while len(heap) > 0:
           res.append(heapq.heappop(heap)[1])
